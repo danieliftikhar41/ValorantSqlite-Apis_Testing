b6e21b69df9ffa1f6ca82df3620d8b9c40cb2617
danieliftikhar41 <danieliftikhar41@gmail.com>
2021-10-21 16:15:03 +0200

    Froum , list update

--- app/src/main/java/com/example/logginvalorant/MainActivity.java
+++ app/src/main/java/com/example/logginvalorant/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(Redrict);
 
                     Log.i("Login","you are inside");
-                }else{
-
                 }
             }}
 
