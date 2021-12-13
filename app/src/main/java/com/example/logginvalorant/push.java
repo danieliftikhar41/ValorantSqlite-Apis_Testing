b83ecf296362c2d408b3a76bba0066c3f2650d74
danieliftikhar41 <danieliftikhar41@gmail.com>
2021-12-13 16:30:45 +0100

    push2

---
+++ app/src/main/java/com/example/logginvalorant/push.java
@@ -0,0 +1,14 @@
+package com.example.logginvalorant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class push extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push);
+    }
+}
